7e9a202f98580b800dc635ce951f2491b0b020b5
Ersel Aker <erselaker@gmail.com>
2018-07-24 17:53:52 +0100

    Update read-file-1.js

--- read-file/read-file-1.js
+++ read-file/read-file-1.js
@@ -3,8 +3,6 @@ const path = require('path');
 
 const filepath = path.join(__dirname, 'hello.txt');
 
-// this will log 1, 3, 2 because the asynchronous readFile will not wait
-
 console.log(1);
 
 fs.readFile(filepath, () => {
